82b936657d5b4e2c2015ba992dc90820a3a4c694
Eduardo-PC <Eduardo-PC@Eduardo-PC>
2021-09-23 13:28:38 -0300

    Controller Usuário Criar Banco de dados e configurar no application.properties
    EndPoint: localhost:8080/v1/usuarios
    
    JSON para Cadastrar
    {
        "nome": "Eduardo",
        "email": "eduardo@vip.com",
        "senha": "123456",
        "dataNascimento": "1989-11-21",
        "sexo": "Masculino",
        "acesso": "2021-09-23T13:23:00Z"
    }

--- Controle-Financeiro/src/main/java/br/com/vipautomacao/ControleFinanceiroApplication.java
+++ Controle-Financeiro/src/main/java/br/com/vipautomacao/ControleFinanceiroApplication.java
@@ -1,14 +1,26 @@
 package br.com.vipautomacao;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import br.com.vipautomacao.core.io.Base64ProtocolResolver;
+import br.com.vipautomacao.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class ControleFinanceiroApplication {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
-		SpringApplication.run(ControleFinanceiroApplication.class, args);
+//		SpringApplication.run(ControleFinanceiroApplication.class, args);
+		
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		
+		var app = new SpringApplication(ControleFinanceiroApplication.class);
+//		app.addListeners(new Base64ProtocolResolver());
+		app.run(args);
 	}
 
 }
