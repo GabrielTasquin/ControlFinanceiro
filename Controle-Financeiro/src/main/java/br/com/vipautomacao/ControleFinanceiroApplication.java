265920057b60ade059616623b9a7361ace172313
Eduardo-PC <Eduardo-PC@192.168.1.101>
2021-09-22 23:53:38 -0300

    Teste2

--- Controle-Financeiro/src/main/java/br/com/vipautomacao/ControleFinanceiroApplication.java
+++ Controle-Financeiro/src/main/java/br/com/vipautomacao/ControleFinanceiroApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ControleFinanceiroApplication {
 
 	public static void main(String[] args) {
+		System.out.println("teste");
 		SpringApplication.run(ControleFinanceiroApplication.class, args);
 	}
 
